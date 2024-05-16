2ef40dee473ea2495455614b69abd62b7875ca1f
TH-Raju <rajukhan894200@gmail.com>
2024-05-16 08:26:10 +0600

    initialize

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+  var navbarLinks = document.querySelector(".navbar-links");
+  navbarLinks.classList.toggle("active");
+});
